dc28e7721fc730375684b403074259717e4f3090
Luca Lit <luca.lit.21@dartmouth.edu>
2021-05-19 16:22:56 -0400

    sa-react-native

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainTabBar from './navigation/main-tab-bar';
+
+// disable really annoying in app warnings
+console.disableYellowBox = true;
+
+const App = (props) => {
+  return <MainTabBar />;
+};
+
+
+export default App;
---
+++ youtube-api.js
@@ -0,0 +1,27 @@
+  
+import axios from 'axios';
+
+const API_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyAL5w37HdVVTUa79kWJ9mdS87UuqeYHWe4';
+
+const youtubeSearch = (term) => {
+  const params = {
+    part: 'snippet',
+    key: API_KEY,
+    q: term,
+    type: 'video',
+  };
+
+  return new Promise((resolve, reject) => {
+    axios.get(API_URL, { params })
+      .then((response) => {
+        resolve(response.data.items);
+      })
+      .catch((error) => {
+        console.log(`youtube api error: ${error}`);
+        reject(error);
+      });
+  });
+};
+
+export default youtubeSearch;
